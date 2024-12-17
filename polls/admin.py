10eadf2489e45f3d3105b94f0f2dd02a9f17eb11
AndjDavis <andi.idavis@gmail.com>
2024-12-17 09:49:46 -0700

    registered models on admin

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Choice, Question
+
+admin.site.register(Choice)
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ("question_text", "date_published")
+
+    def date_published(self, obj):
+        return obj.pub_date.strftime("%Y-%m-%d %I:%M %p")
+    
+    date_published.short_description = "Publication Date"
